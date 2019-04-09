6117507a99c957ddb6e76d9b1b1cefac8849a790
mavan001 <49408205+mavan001@users.noreply.github.com>
2019-04-09 10:56:47 +0200

    Update pqueue.h

--- pqueue.h
+++ pqueue.h
@@ -1,15 +1,26 @@
 #ifndef _PQUEUE_H
 #define _PQUEUE_H
 
-    #define OK = 0;
+#define OK = 0;
+typedef struct pqentry{
+    float priority;
+    char* string;
+    struct pqentry *prev;
+    struct pqentry *next;
+}pqentry_t;
 
-    typedef struct priorityqueue priorityqueue_t;
+// *******************************************************************
+typedef struct priorityqueue {
+    int size;
+    pqentry_t *head;
+    pqentry_t *tail;
+}priorityqueue_t;
 
-    priorityqueue_t* pqueue_create();
-    void pqueue_insert(priorityqueue_t *pq, char* value, float p);
-    char* pqueue_extractMin(priorityqueue_t *pq);
-    void pqueue decreaseKey(priorityqueue t *pq, char* value, float p);
-    void pqueue remove(priorityqueue t *pq, char* value);
-    void pqueue destroy(priorityqueue t *pq);
-    
+priorityqueue_t* pqueue_create();
+void pqueue_insert(priorityqueue_t *pq, char* value, float p);
+char* pqueue_extractMin(priorityqueue_t *pq);
+void pqueue_decreaseKey(priorityqueue_t *pq, char* value, float p);
+void pqueue_remove(priorityqueue_t *pq, char* value);
+void pqueue_destroy(priorityqueue_t *pq);
+
 #endif
